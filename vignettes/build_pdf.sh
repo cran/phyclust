db2b21bf1da34d808c141f61d49c9a008c0a7ce9
Wei-Chen Chen <wccsnow@gmail.com>
2013-12-14 00:00:00 +0000

    version 0.1-15

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -6,8 +6,9 @@ bibtex phyclust-guide
 pdflatex phyclust-guide.Rnw
 pdflatex phyclust-guide.Rnw
 pdflatex phyclust-guide.Rnw
+Rscript -e "tools::compactPDF('.', gs_quality='ebook')"
 rm *.aux *.bbl *.blg *.log *.out *.toc
 
-# mv phyclust-guide.pdf phyclust-guide.pdf.org
-# qpdf phyclust-guide.pdf.org phyclust-guide.pdf
-# rm *.org
+mv -f *.pdf ../inst/doc/
+cp -f *.Rnw ../inst/doc/
+cp -f *.html ../inst/doc/
